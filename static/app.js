9b7f848fb81caa01a24b42d5f8927e6170d8455c
Edson Hilios <edson.hilios@telefonicabeta.com>
2015-01-22 17:42:39 -0200

    Add create and update pipeline

--- static/app.js
+++ static/app.js
@@ -1,54 +1,145 @@
-var app = angular.module('tasks', []);
+var app = angular.module('tasks', ['ngAnimate']);
+// Constants and variables
+app.constant('API_ENDPOINT', 'http://cpbr8app.herokuapp.com')
 // Configuration
-app.value('API_ENDPOINT', 'http://cpbr8app.herokuapp.com')
+app.config(function($sceDelegateProvider, API_ENDPOINT) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    API_ENDPOINT + '/**'
+  ]);
+});
 // Services
 app.factory('Tasks', function($http, API_ENDPOINT) {
 
-  function _buildUrl(path, id) {
-    url = API_ENDPOINT + path;
+  function _urlFor(path, id) {
+    var url = API_ENDPOINT;
 
     if (id) {
-      url = url + '?id=' + id;
+      url = API_ENDPOINT + path +'?id=' + id;
+    } else {
+      url = API_ENDPOINT + path;
     }
 
     return url;
   }
 
   function all() {
-    var url = _buildUrl('/tasks');
+    var url = _urlFor('/tasks');
     return $http.get(url);
   }
 
-  return {
-    'all': all
-  };
-});
-// Controllers
-app.controller('TasksController', function ($scope, Tasks) {
-  $scope.tasks = [];
+  function put(data) {
+    var id, url, q;
+    if (data.hasOwnProperty('id')) {
+      id = data.id;
+    }
 
-  $scope.select = function(model) {
+    if (id) {
+      url = _urlFor('/task', id);
+      q = $http.put(url, data);
+    } else {
+      url = _urlFor('/task');
+      q = $http.post(url, data);
+    }
 
+    return q;
   }
 
-  $scope.refresh = function() {
-    Tasks.all().success(function(data) {
-      $scope.tasks = data.tasks;
+  return {
+    'all': all,
+    'put': put
+  };
+});
+app.factory('Spinner', function($rootScope, $q) {
+
+  function watch() {
+    var promisses = Array.prototype.slice.call(arguments);
+
+    $rootScope.$broadcast('spinner:show');
+
+    $q.all(promisses).finally(function() {
+      $rootScope.$broadcast('spinner:hide');
     });
   }
 
-  $scope.refresh();
+  return {
+    'watch': watch
+  };
 });
-app.controller('TaskController', function ($scope, Tasks) {
+// Controllers
+app.controller('TasksController', function ($scope, Tasks, Spinner) {
+  var q;
 
+  $scope.tasks = [];
+  $scope.currentModel = null;
+
+  function _findById(id) {
+    return function(model) {
+      return model.id == id;
+    }
+  }
+
+  $scope.open = function(model) {
+    $scope.currentModel = model;
+  }
+
+  $scope.save = function(model) {
+    q = Tasks.put(model)
+      .success(function(data) {
+        var isNew = !$scope.tasks.some(_findById(data.id));
+        if (isNew) {
+          $scope.tasks.push(data)
+        }
+      })
+      .finally(function() {
+        $scope.close();
+      });
+
+    Spinner.watch(q)
+  }
+
+  $scope.toggle = function(model) {
+    model.ok = !model.ok;
+    $scope.save()
+  }
+
+  $scope.close = function() {
+    $scope.currentModel = null;
+  }
+
+  $scope.refresh = function() {
+    q = Tasks.all()
+      .success(function(data) {
+        $scope.tasks = data.tasks;
+      });
+
+    Spinner.watch(q);
+  }
+
+  $scope.refresh();
 });
 // Directives
 app.directive('task', function() {
   return {
     templateUrl: 'task.html',
-    require: 'ngModel',
     scope: {
-      'ngModel': '='
+      'ngModel': '=',
+      'onEdit': '&'
+    }
+  };
+});
+app.directive('spinner', function() {
+  return {
+    templateUrl: 'spinner.html',
+    scope: true,
+    link: function(scope, el, attrs) {
+      scope.isLoading = true;
+      scope.$on('spinner:show', function() {
+        scope.isLoading = true;
+      });
+      scope.$on('spinner:hide', function() {
+        scope.isLoading = false;
+      });
     }
   };
 });
