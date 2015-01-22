a4e062d5eecdc140d6daaf9c3962064bd196b790
Edson Hilios <edson.hilios@telefonicabeta.com>
2015-01-22 12:07:15 -0200

    Add the angular and improve the layout

--- static/app.js
+++ static/app.js
@@ -1,1 +1,60 @@
-var app = angular.module('todo', []);
+var app = angular.module('tasks', []);
+// Configuration
+app.value('API_ENDPOINT', 'http://cpbr8app.herokuapp.com')
+// Services
+app.factory('Tasks', function($http, API_ENDPOINT) {
+
+  function _buildUrl(path, id) {
+    url = API_ENDPOINT + path;
+
+    if (id) {
+      url = url + '?id=' + id;
+    }
+
+    return url;
+  }
+
+  function all() {
+    var url = _buildUrl('/tasks');
+    return $http.get(url);
+  }
+
+  return {
+    'all': all
+  };
+});
+// Controllers
+app.controller('TasksController', function ($scope, Tasks) {
+  $scope.tasks = [];
+
+  $scope.refresh = function() {
+    Tasks.all().success(function(data) {
+      $scope.tasks = data.tasks;
+    });
+  }
+
+  $scope.refresh();
+});
+app.controller('TaskController', function ($scope, Tasks) {
+
+});
+// Directives
+app.directive('task', function() {
+  return {
+    templateUrl: 'task.html',
+    require: 'ngModel',
+    scope: {
+      'ngModel': '='
+    }
+  };
+});
+// Filters
+app.filter('len', function() {
+  return function(input) {
+    try {
+      return input.length
+    } catch(e) {
+      return 0
+    }
+  }
+});
