b0441255f92bdbbc9a6d0309d246bf2b9881d376
Edson Hilios <edson.hilios@telefonicabeta.com>
2015-01-23 09:26:41 -0200

    Add http interceptor to handle form data correctly

--- static/app.js
+++ static/app.js
@@ -8,6 +8,56 @@ app.config(function($sceDelegateProvider, API_ENDPOINT) {
     API_ENDPOINT + '/**'
   ]);
 });
+app.config(function($httpProvider) {
+  // Serialize the given Object into a key-value pair string. This method
+  // expects an object and will default to the toString() method.
+  // --
+  // NOTE: This is an atered version of the jQuery.param() method which
+  // will serialize a data collection for Form posting.
+  // --
+  // https://github.com/jquery/jquery/blob/master/src/serialize.js#L45
+  function _formDataSerializer(data) {
+    if (angular.isUndefined(data)) {
+      return;
+    }
+    // If this is not an object, defer to native stringification.
+    if (!angular.isObject(data)) {
+      return((data == null) ? "" : data.toString());
+    }
+    var buffer = [];
+    // Serialize each key in the object.
+    for (var name in data) {
+      if (!data.hasOwnProperty(name)) {
+        continue;
+      }
+      var value = data[name];
+
+      buffer.push(encodeURIComponent(name) + "=" +
+        encodeURIComponent((value == null) ? "" : value)
+      );
+    }
+    // Serialize the buffer and clean it up for transportation.
+    var source = buffer.join("&").replace(/%20/g, "+");
+    console.log(source);
+
+    return source;
+  }
+
+  function _transformRequestDataToForm(data, getHeaders) {
+    if (data) {
+      data = angular.fromJson(data);
+
+      var headers = getHeaders();
+      headers["Content-Type"] = "application/x-www-form-urlencoded";
+
+      return _formDataSerializer(data);
+    }
+
+    return data;
+  }
+
+  $httpProvider.defaults.transformRequest.push(_transformRequestDataToForm);
+});
 // Services
 app.factory('Tasks', function($http, API_ENDPOINT) {
 
